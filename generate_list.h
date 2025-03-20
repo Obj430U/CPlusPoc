d9f324a039fd08b8463a6c1063439804a2b840e1
Ali Hajj Hassan <ali.hajjhassan@montyholding.com>
2025-03-20 15:06:26 +0200

    init commit

---
+++ generate_list.h
@@ -0,0 +1,8 @@
+#ifndef GENERATE_LIST_H
+#define GENERATE_LIST_H
+
+#include <vector>
+
+std::vector<int> generateList(int size, int minVal, int maxVal);
+
+#endif
---
+++ generatelist.cpp
@@ -0,0 +1,14 @@
+#include "generate_list.h"
+#include <cstdlib>
+#include <ctime>
+
+// Function definition
+std::vector<int> generateList(int size, int minVal, int maxVal) {
+    std::vector<int> list;
+    list.reserve(size);
+    for (int i = 0; i < size; ++i) {
+        int randomValue = minVal + (rand() % (maxVal - minVal + 1));
+        list.push_back(randomValue);
+    }
+    return list;
+}
